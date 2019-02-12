cc9b9add95da477b6292d850efce481619c3d2fd
duprasmartin <47566268+duprasmartin@users.noreply.github.com>
2019-02-12 14:40:53 +0000

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>   // this is the library that containc console input and output (e.g. "cout")
+using namespace std;  // this prevents having to write "std::cout" every time
+
+int main() 
+{
+    cout << "Hello, World!";  // print this string to the console
+    return 0;                 // when done, exit and return the value 0, which usually means that no error happened.
+}
