9aa03eea41e2b0e12cca28d895ac257a61529978
duprasmartin <47566268+duprasmartin@users.noreply.github.com>
2019-02-18 18:20:31 +0000

    Add files via upload

---
+++ circle.h
@@ -0,0 +1,15 @@
+class Circle {
+private:
+    float m_radius;
+
+public:
+    float GetRadius();
+
+    float SetRadius();
+
+    float CalculateArea();
+
+    float CalculateCircumference();
+
+    float CaculateDiameter();
+}
