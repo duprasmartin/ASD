b95f00b1bbebfc6a6d68f379a3d62bfe2f56e10c
duprasmartin <47566268+duprasmartin@users.noreply.github.com>
2019-02-28 16:28:01 +0000

    Add files via upload

---
+++ terribleterriblegame/src/ball.h
@@ -0,0 +1,23 @@
+//
+//  ball.h
+//  terribleterriblegame
+//
+//  Created by Martin Dupras on 28/02/2019.
+//
+
+#ifndef ball_h
+#define ball_h
+
+
+#endif /* ball_h */
+
+class Ball {
+public:
+    
+private:
+    ofPoint m_position;
+    int m_ballsize;
+    float m_speedx;
+    float m_speedy;
+    
+}
---
+++ terribleterriblegame/src/ofApp.cpp
@@ -0,0 +1,89 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+// set position and speed of p1_paddle
+    // set position and speed of p2_paddle;
+    // set position and speed of ball
+    // set scores to 0
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    // update paddle1 position
+    // update paddle2 position
+    // update ball position
+    
+    //test for collisions:
+        // test ball against upper and lower walls
+                // if hit, switch vertical speed
+    
+        // test ball against paddles
+                // if hit, switch horizontal speed
+    
+    // else:
+                // give point to other paddle
+                // update score
+                // test if game won
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
